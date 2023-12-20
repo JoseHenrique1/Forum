6106233dbe02a81606df71f7770098b9acad3300
Jose Henrique <jh4832184@gmail.com>
2023-12-20 14:42:26 -0300

    feat: add handleSubmit no cadastro

--- src/app/(autenticacao)/cadastro/page.js
+++ src/app/(autenticacao)/cadastro/page.js
@@ -6,26 +6,44 @@ function Page() {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
 
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let data = {nome, email, senha}
+        const req = await fetch('http://localhost:3000/cadastro',{
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(data=>data.json())
+        .catch(console.error);
+        if (req.msg=="success") {
+            alert('cadastrado com sucesso')  
+        }
+    }
+
     return ( 
         <main>
-            <div>
+            <form onSubmit={handleSubmit}>
                 <input 
                     type="text" 
                     placeholder="Nome" 
                     value={nome} 
-                    onChange={(e)=>{setNome(e.target.value)}}/>
+                    onChange={(e)=>{setNome(e.target.value)}}
+                    required/>
                 <input 
                     type="email" 
                     placeholder="Email@gmail.comm" 
                     value={email} 
-                    onChange={(e)=>{setEmail(e.target.value)}}/>
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                    required/>
                 <input 
                     type="password" 
                     placeholder="Senha" 
                     value={senha} 
-                    onChange={(e)=>{setSenha(e.target.value)}}/>
-                <button>Cadastrar</button>
-            </div>
+                    onChange={(e)=>{setSenha(e.target.value)}}
+                    required/>
+                <input type="submit" value="Cadastre-se" />
+            </form>
         </main>
      );
 }
