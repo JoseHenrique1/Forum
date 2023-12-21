439a2d9b1f3b20af2bba05a0cc04c2ea8d8dbc60
Jose Henrique <jh4832184@gmail.com>
2023-12-21 10:39:45 -0300

    feat: add handleSubmit no login

--- src/app/(autenticacao)/login/page.js
+++ src/app/(autenticacao)/login/page.js
@@ -5,21 +5,38 @@ function Page() {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
 
+    async function handleSubmit(e) {
+        e.preventDefault();
+        let data = {email, senha}
+        const req = await fetch('http://localhost:3000/autenticacao',{
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(data=>data.json())
+        .catch(e=>console.log(e));
+        if (req.msg=="success") {
+            alert('login realizado com sucesso')  
+        }
+    }
+
     return ( 
         <main>
-            <div>
+            <form onSubmit={handleSubmit}>
                 <input 
                     type="email" 
                     placeholder="Email@gmail.comm" 
                     value={email} 
-                    onChange={(e)=>{setEmail(e.target.value)}}/>
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                    required/>
                 <input 
                     type="password" 
                     placeholder="Senha" 
                     value={senha} 
-                    onChange={(e)=>{setSenha(e.target.value)}}/>
-                <button>Cadastrar</button>
-            </div>
+                    onChange={(e)=>{setSenha(e.target.value)}}
+                    required/>
+                <input type="submit" value="Fazer login" />
+            </form>
         </main>
      );
 }
