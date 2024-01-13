30bcba325025939f22924c0d1bbc1743736d5015
Jose Henrique <jh4832184@gmail.com>
2024-01-13 09:21:12 -0300

    feat: add handleSendComment e importacao do useSession

--- src/app/(main)/temas/[id]/page.js
+++ src/app/(main)/temas/[id]/page.js
@@ -1,8 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
 import Comentario from "@/components/Comentario/index.js";
+import { useSession } from "next-auth/react";
 
 function Page({params}) {
+    const { data: session, status } = useSession();
     const [tema, setTema] = useState("");
     const [interacoes, setInteracoes] = useState([]);
     
@@ -22,14 +24,43 @@ function Page({params}) {
 
     useEffect(handleLoadTema,[])
     useEffect(handleLoadInteractions, []);
+
+    async function handleSendComment (e) {
+        e.preventDefault();
+        if (session) {
+            let data = {
+                mensagem : e.target.comment.value,
+                usuarioId : session.user.id,
+                temaId: params.id 
+            }
+            console.log(data)
+            await fetch('http://localhost:3000/comentarios', {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(data=>data.json())
+            .then((data)=>{
+                console.log(data)
+                setInteracoes([...interacoes, {...data.comentario, respostas:[]}])
+            })
+            .catch(e=>console.log(e))
+        }
+        e.target.reset()
+    }
     
     return ( 
         <main>
             <h3>{tema}</h3>
+            <p>{session?.user.nome} faça um comentario</p>
+            <form onSubmit={handleSendComment}>
+                <textarea name="comment" required />
+                <input type="submit" value="Enviar" />
+            </form>
             <section>
                 {
-                    interacoes.map((interacao)=>{
-                        return <Comentario key={interacao.id} comentario={interacao} />
+                    session?.user && interacoes.map((interacao)=>{
+                        return <Comentario key={interacao.id} comentario={interacao} user={session.user} />
                     })
                 }
             </section>   
