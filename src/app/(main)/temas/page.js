81d8b57cbc4d2f088e17c10388f3fb0a2ca7d642
Jose Henrique <jh4832184@gmail.com>
2024-01-28 09:47:46 -0300

    feat: add handleCreateThemes

--- src/app/(main)/temas/page.js
+++ src/app/(main)/temas/page.js
@@ -2,7 +2,10 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+
 function Page() {
+    const { data: session, status } = useSession();
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
     const [themes, setThemes] = useState([]);
 
@@ -12,20 +15,41 @@ function Page() {
         .then(data=>setThemes(data))
         .catch(error=>console.log(error))
     }
+
+    function handleCreateThemes (e) {
+        e.preventDefault();
+        fetch(API_URL+'temas', { 
+            method: "POST", 
+            body: JSON.stringify({
+                usuarioId: session?.user.id,
+                conteudo: e.target.theme.value
+            }),
+            headers: {"Content-type": "application/json; charset=UTF-8"} 
+        })
+        .then(data=>data.json())
+        .then(data=>setThemes(e=>[...e,data.tema]))
+        e.target.reset();
+    }
     useEffect(handleLoadThemes, []);
 
     return ( 
         <main>
             <h1>Temas para debate</h1>
-            {themes.map((theme)=>{
-                const {id, conteudo, usuarioId } = theme;
-                return (
-                    <div key={id} >
-                        <p> {conteudo} </p>
-                        <Link href={"/temas/"+id} >Acessar</Link>
-                    </div>
-                )
-            })}
+            <form onSubmit={handleCreateThemes}>
+                <input type="text" name="theme" required />
+                <input type="submit" value="Criar tema"/>
+            </form>
+            <section>
+                {themes.map((theme)=>{
+                    const {id, conteudo, usuarioId } = theme;
+                    return (
+                        <div key={id} >
+                            <p> {conteudo} </p>
+                            <Link href={"/temas/"+id} >Acessar</Link>
+                        </div>
+                    )
+                })}
+            </section>
         </main>
      );
 }
