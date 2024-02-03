760c07864f3a967d3c0bbcae8268b2f51ff00f38
Jose Henrique <jh4832184@gmail.com>
2024-02-02 23:46:23 -0300

    refactor: comentario agora possui um ContainerRespostas

--- src/components/Comentario/index.js
+++ src/components/Comentario/index.js
@@ -1,72 +1,12 @@
-import { useEffect, useState } from "react";
+"use client";
+import { useState } from "react";
 import Image from "next/image";
-import Resposta from "../Resposta";
-
+import ContainerRespostas from "../ContainerRespostas";
 
 function Comentario({comment, user, socket}) {
-    const [mostrarRespostas, setMostrarRespostas] = useState(false);
-    const [responder, setResponder] = useState(false);
-
-    //tratando dados vindos da api
+    const [show, setShow] = useState(false);
     const {id, mensagem, usuario:{nome, email}} = comment;
-    const [responsesAll, setResponsesAll] = useState([]);
-    const [responses, setResponses] = useState([]);
-    const [responsesNumber, setResponsesNumber] = useState(5);
 
-    function handleLoadResponses () {
-        fetch(`http://localhost:3000/respostas/?comentarioId=${id}`)
-        .then(data=>data.json())
-        .then((data)=>{
-            setResponsesAll(data.respostas);  
-        })
-        .catch(e=>console.log(e))
-    }
-
-    function handleLoadMoreResponses () {
-        setResponses((e)=>{ 
-            if (e.length != 0 && e[e.length-1].id == responsesAll.slice(responsesNumber-5, responsesNumber)[4]?.id) {return e}
-            else { return [...e, ...responsesAll.slice(responsesNumber-5, responsesNumber)] }
-        })
-    }
-
-    async function handleSendResponse (e) {
-        e.preventDefault()
-        let data = {
-            mensagem : e.target.response.value,
-            usuarioId : user.id,
-            comentarioId: id 
-        }
-        let req = await fetch('http://localhost:3000/respostas', {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
-        .then(data=>data.json())
-        .then(data=>data)
-        .catch(e=>console.log(e));
-        setResponses((e)=>{
-            return [{...req.resposta, usuario: {id: user.id, nome: user.nome, email: user.email}}, ...e]
-        })
-        let dataResponse = {
-            ...req.resposta,
-            usuario: {
-                ...user
-            }
-        }
-        socket.current.emit('comentarios', {
-            comentarioId: id, 
-            resposta: dataResponse,
-        });  
-        e.target.reset();
-    }
-    useEffect(()=>{
-        socket.current.on('comentario'+id, (data)=>{
-            setResponses(e=>[data.resposta, ...e])   
-        })
-        return () => {socket.current.disconnect()}
-    },[]);
-    useEffect(handleLoadResponses, []);
-    useEffect(handleLoadMoreResponses, [responsesAll ,responsesNumber]); 
     return ( 
         <div>
             <div>
@@ -75,28 +15,9 @@ function Comentario({comment, user, socket}) {
             </div>
             <p>{mensagem}</p>
             <div>
-                <button onClick={()=>setMostrarRespostas(m=> !m)}>Ver respostas</button>
-                <button onClick={()=>setResponder(r=>!r)}>Responder</button>
-                {
-                    responder && 
-                    <form onSubmit={handleSendResponse}>
-                        <input type="text" name="response" required/>
-                        <input type="submit" value="enviar"/>
-                    </form>
-                }
+                <button onClick={()=>setShow(e=>!e)}>{show? 'Ver menos': 'Ver respostas'}</button>
             </div>
-            <div>
-                {
-                    mostrarRespostas && responses.map((resposta)=>{
-                        return <Resposta key={resposta.id} resposta={resposta} />})  
-                }
-                {
-                    mostrarRespostas && <button 
-                            onClick={()=>setResponsesNumber(e=>e+5)} 
-                            disabled={responsesNumber >= responsesAll.length? true: false}
-                    >Mais respostas</button>
-                }
-            </div>
+            {show && <ContainerRespostas comentarioId={id} socket={socket} user={user} />}
             <hr />
             <hr />
         </div>
@@ -104,3 +25,14 @@ function Comentario({comment, user, socket}) {
 }
 
 export default Comentario;
+
+
+/*
+USER
+MSG - comment
+OPTIONS - buttons
+FORM - send response
+CONTAINER - responses
+
+106 LINHAS
+*/ 
