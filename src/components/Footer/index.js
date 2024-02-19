4b66ba4c06e2658fd578d8bff37abfe10b49bdb3
Jose Henrique <jh4832184@gmail.com>
2024-02-19 10:23:25 -0300

    refactor: footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 function Footer() {
     return ( 
-        <footer>
-            <p>@JoseHenrique1</p>
+        <footer className="bg-blue-400 text-white w-full flex flex-col sm:flex-row justify-center items-center gap-2 border-t p-4 dark:bg-slate-900">
+            <Link href="/" className="shadow"> Github </Link>
+            <Link href="/"> Linkedin </Link>
+            <Link href="/"> Instagram </Link>
         </footer>
      );
 }
